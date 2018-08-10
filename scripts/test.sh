cd9bc313eaf1ea3c7fae1b6b7b56a0499d0ec2a9
Praful Salian <prasalia@publicisgroupe.net>
2018-08-10 14:04:29 +0100

    sdc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,9 @@
-#!bin/sh
-export VERSION_NUMBER="1.25"
-unset VERSION_NUMBER
-if [ -n "$VERSION_NUMBER" ]; then
-    echo "in"
-    echo $VERSION_NUMBER
-    #git tag -af v$VERSION_NUMBER-$TRAVIS_BUILD_NUMBER $TRAVIS_COMMIT -m "Release Tag created by TravisCI"
-    #git push --no-verify --delete origin v$VERSION_NUMBER-$TRAVIS_BUILD_NUMBER
-    #git push --no-verify origin v$VERSION_NUMBER-$TRAVIS_BUILD_NUMBER
+
+export TRAVIS_TAG="v1.40-trial"
+export TRAVIS_PATTERN="^\"?[vV][0-9]+\.[0-9]+(-trial)?\"?$"
+if [[ $TRAVIS_TAG =~ $TRAVIS_PATTERN ]];
+then
+    echo "1"
+else
+    echo "2"
 fi
