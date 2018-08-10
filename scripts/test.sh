920e558936608a531f0d9dd1d1a84b27b419dc21
Praful Salian <prasalia@publicisgroupe.net>
2018-08-10 15:01:34 +0100

    sdc

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
-
-export TRAVIS_TAG="v1.40-trial"
-export TRAVIS_PATTERN="^\"?[vV][0-9]+\.[0-9]+(-trial)?\"?$"
-if [[ $TRAVIS_TAG =~ $TRAVIS_PATTERN ]];
-then
-    echo "1"
-else
-    echo "2"
-fi
+#!/usr/bin/env bash
+BBB="release/v1.35"
+echo $BBB
+if [[ $BBB =~ ^release.*$ ]]; then echo "match"; fi
+#then
+#    echo "match";
+#else
+#    echo "no match";
+#fi
