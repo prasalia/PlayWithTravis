b21056b6e07347effbfc81e0b0891ef641082d17
Praful Salian <prasalia@publicisgroupe.net>
2018-09-07 12:21:47 +0100

    Updated yarn.lock and travis

--- scripts/yarn_cache_clean.sh
+++ scripts/yarn_cache_clean.sh
@@ -8,16 +8,15 @@ echo $TRAVIS_COMMIT_RANGE
 
 rev="$TRAVIS_COMMIT..HEAD^"
 
-if [ "$TRAVIS_COMMIT_RANGE" != "" ];
+if [ "$TRAVIS_COMMIT_RANGE" != "" ]
 then
   rev=$TRAVIS_COMMIT_RANGE
 fi
 
 echo $rev
-
+git --no-pager log -n 5
 git diff-tree --no-commit-id --name-only -r $rev
-
-if git diff-tree --no-commit-id --name-only -r $rev | grep -q yarn.lock > /dev/null; 
+if git diff-tree --no-commit-id --name-only -r $rev | grep -q yarn.lock 
 then
     echo "running yarn cache clean"
 fi
