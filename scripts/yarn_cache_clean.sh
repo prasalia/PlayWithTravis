348fcfcad3696ffc6397ffba9d12ae0677c1931a
Praful Salian <prasalia@publicisgroupe.net>
2018-09-07 12:06:42 +0100

    sdxded

--- scripts/yarn_cache_clean.sh
+++ scripts/yarn_cache_clean.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-TRAVIS_COMMIT="37236fb38745fee9a7fd25e687d5c2246739ad46"
-TRAVIS_COMMIT_RANGE="37236fb38745fee9a7fd25e687d5c2246739ad46..dbd1a30f4c57b157c98925d508b6edc5486e1cd2"
+
 
 
 echo $TRAVIS_COMMIT
@@ -9,13 +8,17 @@ echo $TRAVIS_COMMIT_RANGE
 
 rev="$TRAVIS_COMMIT..HEAD^"
 
-if [ "$TRAVIS_COMMIT_RANGE" == "" ];
+if [ "$TRAVIS_COMMIT_RANGE" != "" ];
 then
-  set rev=$TRAVIS_COMMIT_RANGE
+  rev=$TRAVIS_COMMIT_RANGE
 fi
 
 echo $rev
 
 git diff-tree --no-commit-id --name-only -r $rev
-git diff-tree --no-commit-id --name-only -r $rev | grep -q yarn.lock
+
+if git diff-tree --no-commit-id --name-only -r $rev | grep -q yarn.lock > /dev/null; 
+then
+    echo "running yarn cache clean"
+fi
 
